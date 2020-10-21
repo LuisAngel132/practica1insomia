c9a247ed4969a5f6065ab59f76cff5d045760580
LuisAngel132 <71341676+LuisAngel132@users.noreply.github.com>
2020-10-21 01:24:24 -0500

    rutas agregadas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+<?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\example\MyController;
@@ -16,15 +18,30 @@ use App\Http\Controllers\example\controlador;
 |
 */
 
-Route::post('persona','example\MyController@personas')
-->where (['nombre'=>'[a-z A-Z]+']);
-Route::get('vpersona','example\MyController@personasvista')
-->where (['nombre'=>'[a-z A-Z]+']);
+Route::post('persona','example\MyController@personas');
+Route::get('vpersona','example\MyController@personasvista');
 Route::post('producto','example\controlador@producto');
-Route::get('vproducto','example\MyController@productosvista')
-->where (['nombre'=>'[a-z A-Z]+']);
+Route::get('vproducto','example\MyController@productosvista');
 Route::post('comentarios','example\MyController@comentarios');
 Route::get('vcomentarios','example\MyController@comentariosvista');
+Route::get('personas/{id}/vcomentariosp','example\MyController@getComentariosPorPersona')
+->where (['id'=>'[0-9]+']);
+Route::get('productos/{producto}/vcomentariospr','example\MyController@comentariosvistaporproducto')
+->where (['producto'=>'[0-9]+']);
 
-Route::get('personas/{id}/vcomentariosp','example\MyController@getComentariosPorPersona');
-Route::get('productos/{producto}/vcomentariospr','example\MyController@comentariosvistaporproducto');
+Route ::put ('apersona/{idpersona}','example\MyController@actualizar');
+Route ::delete ('eproducto/{idproducto}','example\MyController@eliminarproducto')
+->where (['idproducto'=>'[0-9]+']);
+Route ::put ('aproducto/{idproducto}','example\MyController@actualizarproducto')
+->where (['idproducto'=>'[0-9]+']);
+Route ::get ('vprcomentario/{idproducto?}','example\MyController@vercomentariosporproducto')
+->where (['idproducto'=>'[0-9]+']);
+Route ::get ('vpcomentario/{idproducto?}','example\MyController@vercomentariosporproducto')
+->where (['idproducto'=>'[0-9]+']);
+Route ::delete ('epersona/{idpersona}','example\MyController@eliminarpersona')
+->where (['idproducto'=>'[0-9]+']);
+Route ::delete ('ecomentario/{idpersona}','example\MyController@eliminarcomentarios')
+->where (['idpersona'=>'[0-9]+']);
+Route ::put ('acomentario/{idpersona}','example\MyController@actualizarcomentariosporpersona')
+->where (['idpersona'=>'[0-9]+']);
+
